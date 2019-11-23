e5ba1bb4c1c361c8e2e9828d50804141de49f644
LiuKay <kayfen@foxmail.com>
2019-11-23 11:40:46 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# spring-kay.security-integration
+Spring Security Integration -- 方便 Spring Security 集成
+
+
+- security-core 核心业务逻辑
+- security-browser 浏览器安全相关
+- security-app App安全相关
+- security-demo Demo 小样
---
+++ security-demo/src/main/java/com/kay/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.kay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author LiuKay
+ * @since 2019/11/23
+ */
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello Spring Security!";
+    }
+}
