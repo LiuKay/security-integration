60b6b2c2f3e09283b5a5436944b9fcfc15998d76
LiuKay <kayfen@foxmail.com>
2019-11-24 22:13:51 +0800

    add test for Aspect oriented programming with Spring

---
+++ security-demo/src/main/java/com/kay/web/aspect/RecordAspect.java
@@ -0,0 +1,30 @@
+package com.kay.web.aspect;
+
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author LiuKay
+ * @since 2019/11/24
+ */
+@Aspect
+@Component
+public class RecordAspect {
+
+    @Around(value = "execution(* com.kay.web.controller.UserController.*(..))")
+    public Object around(ProceedingJoinPoint pjp) throws Throwable {
+        System.out.println(">>RecordAspect");
+        Object[] args = pjp.getArgs();
+        for (Object arg : args) {
+            System.out.println("arg is :" + arg);
+        }
+        Object proceed = pjp.proceed();
+        System.out.println(ReflectionToStringBuilder.toString(proceed, ToStringStyle.MULTI_LINE_STYLE));
+        System.out.println("<<RecordAspect");
+        return proceed;
+    }
+}
