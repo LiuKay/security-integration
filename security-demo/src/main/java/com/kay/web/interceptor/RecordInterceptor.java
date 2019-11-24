37f64b1920d544b7339e3bba03231cfbf05a7fff
LiuKay <kayfen@foxmail.com>
2019-11-24 21:57:38 +0800

    fix interceptor test

--- security-demo/src/main/java/com/kay/web/interceptor/RecordInterceptor.java
+++ security-demo/src/main/java/com/kay/web/interceptor/RecordInterceptor.java
@@ -17,30 +17,41 @@ import java.time.LocalDateTime;
 public class RecordInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        HandlerMethod handlerMethod = (HandlerMethod) handler;
-        String className = handlerMethod.getBean().getClass().getName();
-        String methodName = handlerMethod.getMethod().getName();
-        System.out.println("RecordInterceptor preHandle at " + LocalDateTime.now().toString() + ".\n" +
-                "handle :" + className + " " + methodName);
+        System.out.println("---------------RecordInterceptor start---------------------------");
+        if (handler instanceof HandlerMethod) {
+            HandlerMethod handlerMethod = (HandlerMethod) handler;
+            String className = handlerMethod.getBean().getClass().getName();
+            String methodName = handlerMethod.getMethod().getName();
+            System.out.println("RecordInterceptor preHandle at " + LocalDateTime.now().toString() + " for handler :" + className + " " + methodName);
+        } else {
+            System.out.println("RecordInterceptor preHandle handler:" + handler.getClass().getName());
+        }
         return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        HandlerMethod handlerMethod = (HandlerMethod) handler;
-        String className = handlerMethod.getBean().getClass().getName();
-        String methodName = handlerMethod.getMethod().getName();
-        System.out.println("RecordInterceptor postHandle at " + LocalDateTime.now().toString() + ".\n" +
-                "handle :" + className + " " + methodName);
+        if (handler instanceof HandlerMethod) {
+            HandlerMethod handlerMethod = (HandlerMethod) handler;
+            String className = handlerMethod.getBean().getClass().getName();
+            String methodName = handlerMethod.getMethod().getName();
+            System.out.println("RecordInterceptor postHandle at " + LocalDateTime.now().toString() + " for handler :" + className + " " + methodName);
+        } else {
+            System.out.println("RecordInterceptor postHandle handler:" + handler.getClass().getName());
+        }
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        HandlerMethod handlerMethod = (HandlerMethod) handler;
-        String className = handlerMethod.getBean().getClass().getName();
-        String methodName = handlerMethod.getMethod().getName();
-        System.out.println("RecordInterceptor afterCompletion at " + LocalDateTime.now().toString() + ".\n" +
-                "handle :" + className + " " + methodName);
-        System.out.println("ex is " + ex);
+        if (handler instanceof HandlerMethod) {
+            HandlerMethod handlerMethod = (HandlerMethod) handler;
+            String className = handlerMethod.getBean().getClass().getName();
+            String methodName = handlerMethod.getMethod().getName();
+            System.out.println("RecordInterceptor afterCompletion at " + LocalDateTime.now().toString() + "for handler :" + className + " " + methodName);
+            System.out.println("ex is " + ex);
+        } else {
+            System.out.println("RecordInterceptor afterCompletion handler:" + handler.getClass().getName());
+        }
+        System.out.println("---------------RecordInterceptor end---------------------------");
     }
 }
