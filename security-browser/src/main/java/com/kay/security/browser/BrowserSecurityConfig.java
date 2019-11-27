7d5cd4f90247bf4871b7ca47fcb7d833f296b7a3
LiuKay <kayfen@foxmail.com>
2019-11-27 22:05:05 +0800

    custom user authentication process, password encoder

--- security-browser/src/main/java/com/kay/security/browser/BrowserSecurityConfig.java
+++ security-browser/src/main/java/com/kay/security/browser/BrowserSecurityConfig.java
@@ -1,8 +1,11 @@
-package com.kay;
+package com.kay.security.browser;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * @author LiuKay
@@ -13,11 +16,16 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        http.formLogin()
-        http.httpBasic()
+        http.formLogin()
+//        http.httpBasic()
                 .and()
                 .authorizeRequests()
                 .anyRequest()
                 .authenticated();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
---
+++ security-browser/src/main/java/com/kay/security/browser/MyUserDetailsService.java
@@ -0,0 +1,43 @@
+package com.kay.security.browser;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author LiuKay
+ * @since 2019/11/27
+ */
+@Component
+@Log4j2
+public class MyUserDetailsService implements UserDetailsService {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public MyUserDetailsService(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // TODO load user by username from DB and check user status,such as locked, enable..
+
+        // NOTE: this encodePassword should be queried from Database.
+        String encodePassword = passwordEncoder.encode("123456");
+        log.info("Password is :{}", encodePassword);
+
+        return User.builder()
+                .username(username)
+                .password(encodePassword)
+                .authorities(AuthorityUtils.commaSeparatedStringToAuthorityList("admin"))
+                .build();
+    }
+
+}
